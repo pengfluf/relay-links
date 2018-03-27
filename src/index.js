6fcd2b66b7f7bb6ec54ab65f89f44e1dabc17393
user <pengfluf@tutanota.com>
2018-03-27 14:31:18 +0300

    Old Code Deleted

--- src/index.js
+++ src/index.js
@@ -2,73 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { ApolloLink, split } from 'apollo-client-preset';
-import { WebSocketLink } from 'apollo-link-ws';
-import { getMainDefinition } from 'apollo-utilities';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
 // Global styles
 import 'normalize.css';
 import './styles/global.css';
 
 import App from './components/App';
 
-import { AUTH_TOKEN } from './components/Login/constants';
-
 import registerServiceWorker from './registerServiceWorker';
-
-const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
-
-const wsLink = new WebSocketLink({
-  uri: 'ws://localhost:4000',
-  options: {
-    reconnect: true,
-    connectionParams: {
-      authToken: localStorage.getItem(AUTH_TOKEN),
-    },
-  },
-});
-
-const middlewareAuthLink = new ApolloLink((operation, forward) => {
-  const token = localStorage.getItem(AUTH_TOKEN);
-  const authorizationHeader =
-    token ? `Bearer ${token}` : null;
-  operation.setContext({
-    headers: {
-      authorization: authorizationHeader,
-    },
-  });
-  return forward(operation);
-});
-
-const httpLinkWithAuthToken =
-  middlewareAuthLink
-    .concat(httpLink);
-
-const link = split(
-  ({ query }) => {
-    const { kind, operation } = getMainDefinition(query);
-    return kind === 'OperationDefinition'
-            &&
-            operation === 'subscription';
-  },
-  wsLink,
-  httpLinkWithAuthToken
-);
-
-const client = new ApolloClient({
-  link,
-  cache: new InMemoryCache(),
-});
 
 ReactDOM.render(
   <BrowserRouter>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </BrowserRouter>,
   document.getElementById('root')
 );

f5d6636fe432134314c8723c8da5b4f84b0d9945
user <pengfluf@tutanota.com>
2018-03-30 12:19:27 +0300

    Relay Environment Instantiated, Full Schema Downloaded

---
+++ src/Environment.js
@@ -0,0 +1,27 @@
+const {
+  Environment,
+  Network,
+  RecordSource,
+  Store,
+} = require('relay-runtime');
+
+const store = new Store(new RecordSource());
+
+const network = Network.create((operation, variables) => fetch('https://api.graph.cool/simple/v1/cjf9jrd6414oo01439cr3guz2', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    query: operation.text,
+    variables,
+  }),
+}).then((res) => res.json()));
+
+const environment = new Environment({
+  network,
+  store,
+});
+
+export default environment;
